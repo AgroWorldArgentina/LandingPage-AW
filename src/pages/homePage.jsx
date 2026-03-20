a65b967504f359e106fce1711aef3f69464a359c
LucianaGallardoP <lgallardop02@gmail.com>
2026-03-20 02:26:22 -0300

    Cambiar favIcon y titulo pagina. Crear seccion quienes somos con imagen y estilos.

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -5,6 +5,8 @@ import slide1 from "../assets/slide1.jpg";
 import slide2 from "../assets/slide2.jpg";
 import slide3 from "../assets/slide3.jpg";
 import slide4 from "../assets/slide4.jpg";
+import fondoQS from "../assets/fondoQS.jpg";
+import imgQS from "../assets/imgQS.JPG";
 
 export default function HomePage() {
   return (
@@ -15,10 +17,10 @@ export default function HomePage() {
             <p>¿TE INTERESA VENDER</p>
             <p>TUS LOTES DE POROTOS?</p>
           </h1>
-          <h3>
+          <h4>
             <p>Enviá la información de tu lote y nuestro equipo evaluará</p>
             <p>la propuesta para su posible comercialización.</p>
-          </h3>
+          </h4>
           <button id="btnOfreceTuLote">OFRECÉ TU LOTE</button>
         </div>
         <Carousel fade>
@@ -35,6 +37,34 @@ export default function HomePage() {
             <img src={slide4} alt="slide4" width={"100%"} />
           </Carousel.Item>
         </Carousel>
+      </section>
+
+      <section id="seccionQuienesSomos">
+        <img src={fondoQS} alt="fondo ilustraciones 1" width={"100%"} />
+
+        <div id="QS-Container">
+          <div id="textoQS-Container">
+            <h2>¿Quiénes Somos?</h2>
+            <p style={{color:"#377448", fontSize:"large"}}>
+              Agro World Argentina es una empresa dedicada al procesamiento y
+              exportación de porotos desde el año 1996.
+            </p>
+            <p >
+              Contamos con plantas de procesamiento y una flota de más de ocho
+              camiones que nos permite retirar y acopiar la mercadería
+              directamente desde su origen. Posteriormente, el producto es
+              procesado y preparado para su exportación.
+            </p>
+            <p >
+              Nos enfocamos en mantener altos estándares de calidad, cumpliendo
+              con los requisitos y normativas de los mercados internacionales.
+            </p>
+          </div>
+
+          <div>
+            <img src={imgQS} alt="imagen quienes somos" width={"550px"} />
+          </div>
+        </div>
       </section>
     </section>
   );
